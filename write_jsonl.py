f5e3e62bd68f09f31be27f0ecf418034939b40a1
kang <kang6@ewhain.net>
2024-03-19 21:45:40 +0900

    chore: set jsonl file

--- write_jsonl.py
+++ write_jsonl.py
@@ -1,7 +1,7 @@
 import json
 from collections import OrderedDict
 from openai import OpenAI
-
+client = OpenAI()
 
 instruction = """
 SYSTEM:
@@ -328,8 +328,12 @@ data = [
   
 ]
 
-
 with open("train.jsonl", "w", encoding="utf-8") as f:
   for dat in data:
     json.dump(dat, f, ensure_ascii=False) # ensure_ascii로 한글이 깨지지 않게 저장
     f.write("\n")
+
+client.files.create(
+  file=open("train.jsonl", "rb"),
+  purpose="fine-tune"
+)
